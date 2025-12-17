83507fe5aca92e7a722f8c64eb87ee75ced0afa8
Gregoire Pailler <gregoire@pailler.fr>
2025-12-17 22:37:51 +0700

    feat: remove unused errors and http types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,1 @@
-export { HTTPError, LFSError } from "./errors.js";
 export { isValidOID, isValidSize, parseAllowedOrgs, validateOrganization } from "./validation.js";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,4 @@
 export type { AuthResult, GitHubRepository, PermissionLevel } from "./github.js";
-export type { HTTPErrorOptions, LFSErrorResponse } from "./http.js";
 export type {
   LFSAction,
   LFSBatchRequest,
